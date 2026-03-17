3dd54ad8fda0fb89aeac8f2a3b194655c27123d9
yaroslav8765 <pechorkin2014@gmail.com>
2026-03-17 10:48:45 +0200

    fix: extend filter types to include IAdminForthAndOrFilter

--- types.ts
+++ types.ts
@@ -1,13 +1,13 @@
-import { Filters, type IAdminForthSingleFilter } from "adminforth"
+import { Filters, type IAdminForthSingleFilter, type IAdminForthAndOrFilter } from "adminforth"
 interface IFilter {
     name: string;
     icon?: string;
     enum?: {
         label: string;
         icon?: string;
-        filters: () =>  IAdminForthSingleFilter | Promise<IAdminForthSingleFilter>;
+        filters: () =>  IAdminForthSingleFilter | IAdminForthAndOrFilter | Promise<IAdminForthSingleFilter | IAdminForthAndOrFilter>;
     }[]; 
-    searchInput?: (searchVal: string) =>  IAdminForthSingleFilter | Promise<IAdminForthSingleFilter>;
+    searchInput?: (searchVal: string) =>  IAdminForthSingleFilter | IAdminForthAndOrFilter | Promise<IAdminForthSingleFilter | IAdminForthAndOrFilter>;
 }
 export interface PluginOptions {
     filters: IFilter[]
